933a003f8440c92afe59e3c13ce14bbec9710a97
rajang <rajang@dewsolutions.in>
2025-05-14 17:30:09 +0530

    format

--- lib/bloc/app_bloc_observer.dart
+++ lib/bloc/app_bloc_observer.dart
@@ -1,14 +1,34 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppBlocObserver extends BlocObserver{
+class AppBlocObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     print(bloc);
     super.onCreate(bloc);
   }
-@override
+
+  @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
   }
 
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+  }
 }
