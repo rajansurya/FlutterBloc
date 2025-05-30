21556f1894c620d1cfd8db5003a09a49e4969bd4
rajang <rajang@dewsolutions.in>
2025-05-30 15:46:19 +0530

    api call dio

--- lib/repo/PostRepositoryImpl.dart
+++ lib/repo/PostRepositoryImpl.dart
@@ -1,23 +1,37 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
 import 'package:state_bloc/get_it/get_it.dart';
 import 'package:state_bloc/repo/repointerface/PostRepository.dart';
 
 import '../models/Post.dart';
-import 'package:http/http.dart' as http;
 
-@LazySingleton(as:PostRepository)/* This is for singleton design pattern object provider*/
-class PostRepositoryImpl implements PostRepository {
-     /*Access the Module instance*/
-   final http.Client httpClient=getItInstance<http.Client>();
+@LazySingleton(as: PostRepository)
+/* This is for singleton design pattern object provider*/ class PostRepositoryImpl
+    implements PostRepository {
+  /*Access the Module instance*/
+  final httpClient = getItInstance<http.Client>();
+  final dioClient = getItInstance<Dio>();
 
   @override
   Future<List<Post>> fetchPosts() async {
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-    print("Che11==== ${response.body}");
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body) as List;
+    final response = await httpClient.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      headers: {
+        'Accept': 'application/json',
+      },
+    );
+    final responseDio = await dioClient.get('https://jsonplaceholder.typicode.com/posts',options: Options(
+      headers: {
+        'Accept': 'application/json',
+      }
+    ));
+   print("Che11==== ${responseDio.data}");
+    if (responseDio.statusCode == 200) {
+      // final data = jsonDecode(responseDio.data) as List;
+      final data =responseDio.data as List;
       return data.map((e) => Post.fromJson(e)).toList();
     } else {
       throw Exception('Failed to load posts');
